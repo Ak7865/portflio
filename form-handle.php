4c82d273af6b55b9d565282a704a699138443ab6
Syed Akhter hussain <103268023+Ak7865@users.noreply.github.com>
2024-12-24 14:15:25 +0530

    Update form-handle.php

--- form-handle.php
+++ form-handle.php
@@ -1,40 +1,14 @@
+
 <?php
-$name =$_POST['name'];
-$visitor_email =$_POST['email'];
-// $subject =$_POST['subject'];
-$message =$_POST['message'];
-
-$email_from ='WEBSITE EMAIL';
-
-$email_subject ='New Form Submission';
-
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                //  "Subject: $subject.\n".
-                 "User Message: $message.\n";
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
 
 
-$to = 'syedakhterhussain9@gmail.com';
-
-$headers = "From: /storage/ssd1/395/19754395/public_html/form-handler.php";
-
-$headers = "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_body,$headers);
-
-header("Location: #contact");
-
-?>
-<?php
-require 'PHPMailer/Exception.php';
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
-
-
-require 'vendor/autoload.php';
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+// require 'vendor/autoload.php';
+use PHPMailer\SMTP;
+use PHPMailer\PHPMailer;
+use PHPMailer\Exception;
 
 
 
