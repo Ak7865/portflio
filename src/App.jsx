73a42bb5bb4883fc2ef4dafee54e12490c48271a
Ak7865 <ah076145@gmail.com>
2025-11-25 07:35:54 +0530

    React portfolio project1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -21,6 +22,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
+      <Analytics />
       <Footer />
     </div>
   );
